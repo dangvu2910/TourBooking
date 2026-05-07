2d9c6f119045dc83125041795e3023b734611102
dangvu <148321391+dangvu2910@users.noreply.github.com>
2026-05-07 08:58:56 +0700

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Tourbooking.Models;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Tour> Tours { get; set; }
+}
